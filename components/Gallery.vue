704b0840b54b5b4bb0fe348a2a9891664f0cceb9
krajicj <krajicj@gmail.com>
2021-05-13 12:26:32 +0200

    Carousel vs lightbox error fix by downgrade carousel + design changes

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -9,25 +9,25 @@
       layout="fixed-height"
       type="carousel"
     >
-      <amp-img
-        v-for="image in images"
-        :key="image"
-        :srcset="require(`~/assets${image}`).srcSet"
-        width="300"
-        height="200"
-        :alt="title"
-        on="tap:lightbox"
-        role="button"
-        tabindex="0"
-      ></amp-img>
+        <amp-img
+          v-for="image in images"
+          :key="image"
+          :srcset="require(`~/assets${image}`).srcSet"
+          width="300"
+          height="200"
+          :alt="title"
+          on="tap:lightbox"
+          role="button"
+          tabindex="0"          
+        ></amp-img>                  
     </amp-carousel>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['images', 'title', 'heading']
-}
+  props: ["images", "title", "heading"],
+};
 </script>
 
 <style>
