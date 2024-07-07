5b30d4d85cc260b03945b6da9d65d6fa2f10b393
krajicj <krajicj@gmail.com>
2024-07-07 19:59:05 +0200

    Price change in bubbles

--- components/Price.vue
+++ components/Price.vue
@@ -6,15 +6,15 @@
       <div class="price-cards">
         <div class="price-card">
           <div class="price-card-header"><h3>Nová modeláž nehtů</h3></div>
-          <div class="price-card-footer">750 Kč</div>
+          <div class="price-card-footer">800 Kč</div>
         </div>
         <div class="price-card">
           <div class="price-card-header"><h3>Gel lak / Shellac</h3></div>
-          <div class="price-card-footer">580 Kč</div>
+          <div class="price-card-footer">630 Kč</div>
         </div>
         <div class="price-card">
           <div class="price-card-header"><h3>Doplnění modeláže</h3></div>
-          <div class="price-card-footer">610 Kč</div>
+          <div class="price-card-footer">660 Kč</div>
         </div>
       </div>
       <a href="/cenik" class="button btn-center">Kompletní ceník</a>
